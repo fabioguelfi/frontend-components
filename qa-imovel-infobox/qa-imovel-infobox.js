9b18952a211a5c129ae712957c536d11063a1b88
Mauricio <Mauricio>
2015-12-03 18:03:52 -0200

    removendo chamada chumbada

--- qa-imovel-infobox/qa-imovel-infobox.js
+++ qa-imovel-infobox/qa-imovel-infobox.js
@@ -32,7 +32,7 @@ Polymer(
 	load: function() {
 		
 		var self = this;
-		$.getJSON('http://crmlocal.forno.quintoandar.com.br:10020/api/properties/' + self.imovelId, function(json, textStatus)
+		$.getJSON(PROPERTIES_URL + '/api/properties/' + self.imovelId, function(json, textStatus)
 		{
 
 			for(i=0; i<json.imovel.amenidades.length; i++){
